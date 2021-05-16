87f4b0f5bdc7106e67dcb54a35db41c14a06cbbe
Yannick <wgyannick@gmail.com>
2021-05-16 22:44:50 +0200

    Added carousels text box + pagination 👍

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", function () {
-  const img = document.querySelector(".carousel").querySelectorAll("img");
+  const img = document
+    .querySelector(".carousel")
+    .querySelectorAll(".carousel-item");
+  const pagination = document
+    .querySelector(".carousel-pagination")
+    .querySelectorAll(".pagination-item");
   const next = document
     .querySelector(".carousel")
     .querySelector("i.icon-arrow-next");
@@ -12,22 +17,26 @@ window.addEventListener("load", function () {
   window.setInterval(autoPlay, 5000);
 
   function autoPlay() {
-    actualIndex >= img.length - 1 ? (actualIndex = 0) : (actualIndex += 1);
-    img[
-      actualIndex - 1 < 0 ? img.length - 1 : actualIndex - 1
-    ].classList.remove("active");
+    img[actualIndex].classList.remove("active");
+    pagination[actualIndex].classList.remove("active");
+    actualIndex >= img.length - 1 ? (actualIndex = 0) : actualIndex++;
     img[actualIndex].classList.add("active");
+    pagination[actualIndex].classList.add("active");
   }
 
   next.addEventListener("click", function () {
     img[actualIndex].classList.remove("active");
+    pagination[actualIndex].classList.remove("active");
     actualIndex >= img.length - 1 ? (actualIndex = 0) : actualIndex++;
     img[actualIndex].classList.add("active");
+    pagination[actualIndex].classList.add("active");
   });
 
   back.addEventListener("click", function () {
     img[actualIndex].classList.remove("active");
+    pagination[actualIndex].classList.remove("active");
     actualIndex <= 0 ? (actualIndex = img.length - 1) : actualIndex--;
     img[actualIndex].classList.add("active");
+    pagination[actualIndex].classList.add("active");
   });
 });

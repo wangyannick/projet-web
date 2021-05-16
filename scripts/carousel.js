1b2ea1cfb5f758ed68bcbd79eef965961b4b46d1
Yannick <wgyannick@gmail.com>
2021-05-16 22:50:59 +0200

    Refacto duplicate of codes. ✅

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -14,29 +14,28 @@ window.addEventListener("load", function () {
 
   let actualIndex = 0;
 
-  window.setInterval(autoPlay, 5000);
+  window.setInterval(nextSlide, 5000);
 
-  function autoPlay() {
+  function nextSlide() {
     img[actualIndex].classList.remove("active");
     pagination[actualIndex].classList.remove("active");
     actualIndex >= img.length - 1 ? (actualIndex = 0) : actualIndex++;
     img[actualIndex].classList.add("active");
     pagination[actualIndex].classList.add("active");
   }
-
-  next.addEventListener("click", function () {
-    img[actualIndex].classList.remove("active");
-    pagination[actualIndex].classList.remove("active");
-    actualIndex >= img.length - 1 ? (actualIndex = 0) : actualIndex++;
-    img[actualIndex].classList.add("active");
-    pagination[actualIndex].classList.add("active");
-  });
-
-  back.addEventListener("click", function () {
+  function previousSlide() {
     img[actualIndex].classList.remove("active");
     pagination[actualIndex].classList.remove("active");
     actualIndex <= 0 ? (actualIndex = img.length - 1) : actualIndex--;
     img[actualIndex].classList.add("active");
     pagination[actualIndex].classList.add("active");
+  }
+
+  next.addEventListener("click", function () {
+    nextSlide();
+  });
+
+  back.addEventListener("click", function () {
+    previousSlide();
   });
 });

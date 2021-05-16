56719743f21619577b771ce1909eb8dfc8558b9d
Yannick <wgyannick@gmail.com>
2021-05-16 21:55:31 +0200

    Added button on carousel, website is now responsive ✅.

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,14 +1,33 @@
 window.addEventListener("load", function () {
   const img = document.querySelector(".carousel").querySelectorAll("img");
-  let actualIndex = 0;
-  var intervalID = window.setInterval(myCallback, 5000);
+  const next = document
+    .querySelector(".carousel")
+    .querySelector("i.icon-arrow-next");
+  const back = document
+    .querySelector(".carousel")
+    .querySelector("i.icon-arrow-back");
 
-  function myCallback() {
+  let actualIndex = 0;
+
+  var intervalID = window.setInterval(autoPlay, 5000);
+
+  function autoPlay() {
     actualIndex >= img.length - 1 ? (actualIndex = 0) : (actualIndex += 1);
-    console.log(actualIndex);
     img[
       actualIndex - 1 < 0 ? img.length - 1 : actualIndex - 1
     ].classList.remove("active");
     img[actualIndex].classList.add("active");
   }
+
+  next.addEventListener("click", function () {
+    img[actualIndex].classList.remove("active");
+    actualIndex >= img.length - 1 ? (actualIndex = 0) : actualIndex++;
+    img[actualIndex].classList.add("active");
+  });
+
+  back.addEventListener("click", function () {
+    img[actualIndex].classList.remove("active");
+    actualIndex <= 0 ? (actualIndex = img.length - 1) : actualIndex--;
+    img[actualIndex].classList.add("active");
+  });
 });
